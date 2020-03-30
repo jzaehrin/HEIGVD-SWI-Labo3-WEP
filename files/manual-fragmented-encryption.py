34ac8f42b99ee81bbcb3e32e3903b090eb689e98
Jonathan Zaehringer <jonathan.zaehringer@gmail.com>
2020-03-30 16:51:45 +0200

    Clean comments

--- files/manual-fragmented-encryption.py
+++ files/manual-fragmented-encryption.py
@@ -25,10 +25,8 @@ else: # Reading file spliting every '\n'
     print("\tmessage has [0-9A-F]{2}((:)?[0-9A-F]{2})*")
     exit(-1)
 
-# Découpage du message en n fragment (Attention la fragmentation en bloc de moins de 32 bits ne fonctionne pas)
+# Split le message en n fragment de 36 Bytes. valeur 36 marche avec le packet 'template', possiblement à cause de la taille défini dans celui-ci.
 frag_len=36
-
-
 fragments = [message[i * frag_len: min((i + 1) * frag_len, len(message))] for i in range(0, n)]
 
 #lecture de message chiffré - rdpcap retourne toujours un array, même si la capture contient un seul paquet
@@ -41,8 +39,6 @@ packets = []
 seed = template.iv+key
 
 for i, fragment in enumerate(fragments):
-    print(fragment)
-    print(len(fragment))
     # Creation du streamcipher départ pour chaque fragment
     cipher = RC4(seed, streaming=True)
 
@@ -54,16 +50,18 @@ for i, fragment in enumerate(fragments):
     if i != len(fragments) - 1:
         packet.FCfield |= 0x4
     else:
-        fragment += b'\0' * (frag_len - len(fragment))
+        fragment += b'\0' * (frag_len - len(fragment)) # Padding de zero jusqu'à frag_len
         packet.FCfield &= ~0x4
+
     # Creation de la checksum du message
     crc = struct.pack('<L', binascii.crc32(fragment))
+
     # chiffre le message et le change dans la trame
     cipher = cipher.crypt(fragment + crc)
     packet.wepdata = cipher[:-4]
+
     # chiffre l'ICV et le stock dans la trame en int
     packet.icv = struct.unpack('!L', cipher[-4:])[0]
-    print("icv: %s" % binascii.crc32(fragment))
 
     # Ajout du fragment
     packets.append(packet)
